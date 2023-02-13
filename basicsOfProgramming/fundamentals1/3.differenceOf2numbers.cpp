bea7f8363ecbef30ae0c431d70c46085cf3e85d5
“varun” <coldvks@gmail.com>
2023-02-14 01:34:43 +0530

    basic operations on two numbers

---
+++ basicsOfProgramming/fundamentals1/3.differenceOf2numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x, y;
+  int difference;
+  cout << "Type a number: ";
+  cin >> x;
+  cout << "Type another number: ";
+  cin >> y;
+  difference = x - y;
+  cout << "Difference is: " << difference << endl;
+}
---
+++ basicsOfProgramming/fundamentals1/4.productOf2numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x, y;
+  int product;
+  cout << "Type a number: ";
+  cin >> x;
+  cout << "Type another number: ";
+  cin >> y;
+  product = x * y;
+  cout << "Product is: " << product << endl;
+}
