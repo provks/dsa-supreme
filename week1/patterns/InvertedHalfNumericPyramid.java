0429f65a7b0ec08cdaf9f68b842b4f4fbff2bf56
varun <coldvks@gmail.com>
2023-03-21 17:14:55 +0530

    add half numeric pyramid and inverted half numeric pyramid pattern

---
+++ week1/patterns/InvertedHalfNumericPyramid.java
@@ -0,0 +1,19 @@
+package week1.patterns;
+
+import java.util.Scanner;
+
+public class InvertedHalfNumericPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+    
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n-i; j++) {
+                System.out.print(j+1 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
