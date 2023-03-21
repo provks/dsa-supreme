c4ba2f88047392859841abc67899518ae938763b
varun <coldvks@gmail.com>
2023-03-21 16:04:56 +0530

    add hollow rectanlge pattern

---
+++ week1/patterns/HollowRectangle.java
@@ -0,0 +1,20 @@
+package week1.patterns;
+
+import java.util.Scanner;
+
+class HollowRectanlge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<2*n-1; j++) {
+                if (j==0 || i==0 || j==2*n-2 || i==n-1)
+                    System.out.print("* ");
+                else 
+                    System.out.print("  ");
+            }
+            System.out.println("");
+        }
+    }
+
+}
