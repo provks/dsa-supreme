dde4666204792cc37e8b2b16f879aa972d40bf79
varun <coldvks@gmail.com>
2023-06-01 23:59:31 +0530

    add intersection of two arrays

---
+++ week3Array/lec2/IntersectionOfTwoArrays.java
@@ -0,0 +1,32 @@
+package week3Array.lec2;
+
+import java.util.Scanner;
+
+public class IntersectionOfTwoArrays {
+    // find common elements in two arrays (BRUTE FORCE)
+    public static void main(String[] args) {
+        System.out.println("provide arr1 size");
+        int arr1[] = takeArrayInput();        
+        System.out.println("provide arr2 size");
+        int arr2[] = takeArrayInput(); 
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                if (arr1[i] == arr2[j]) {
+                    System.out.print(arr1[i] + " ");
+                    arr2[j] = Integer.MIN_VALUE;    // to handle duplicates
+                }
+            }
+        }
+    }
+
+    private static int[] takeArrayInput() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Enter " + n + " elements"  );
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+}
